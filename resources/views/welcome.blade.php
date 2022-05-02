0634f08e2a854394963b3bb14d1835a169938ec9
PG210 <pedrocuasquer21095@gmail.com>
2022-05-02 14:46:07 -0500

    correcciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
         <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
         <!-- SimpleLightbox plugin CSS-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{ asset('dist/css/styles.css')}}" rel="stylesheet" />
@@ -25,45 +26,42 @@
     <body id="page-top">
         <div >
             <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" style="background-color: white">
-                <div class="container px-4 px-lg-4">
-                    <a class="navbar-brand" href="#page-top">
-                        <img  src="{{ asset('dist/assets/img/portfolio/logob.png')}}" width="100px" height="50px" alt="..." />
-                    </a>
+                <div class="container px-2 px-lg-2">
+                    <a class="navbar-brand" href="https://www.evolucion.co/">
+                       
+                          <img  class="figure-img img-fluid rounded" src="{{ asset('dist/assets/img/portfolio/logob.png')}}" alt="..." />
+      
+                      </a>
                     <!--<button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     -->
-                      <div class="collapse navbar-collapse" id="navbarResponsive">
-                      <!--  <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                            <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                        </ul>-->
-                    </div>
                 </div>
             </nav>
         </div>
         <!-- Navigation-->
        
         <!-- Masthead-->
-        <br><br><br>
         <header class="masthead">
-            <!---#######################-->
+            <!---#######################text-start-->
+            <div class="container-fluid" style="margin-top:8%;">
             <div class="row">
-                <div class="col-lg-8">
-                    <div class="container px-4 px-lg-5 h-100">
-                        <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-                            <div class="col-lg-8 align-self-end">
+                <div class="col-lg-7">
+                    <div class="container-fluid">
+                        <div class="align-items-center">
+                            <div class="container">
                                 <br><br>
-                                <h1 class="text-white font-weight-bold" ><font color="#66efa5" id="titulo">¿Un METAVERSO en mi organización?</font></h1>
+                                <h1 class="font-weight-bold text-start"  ><font color="#66efa5" id="titulo">¿Un METAVERSO en mi organización?</font></h1>
                             </div>
-                            <div class="col-lg-8 align-self-baseline">
-                                <p class="text-white-75 mb-5" id="titulo"><font id="titulo" size="6">Todo lo que necesita saber sobre los <b>Metaversos Empresariales</b></font> </p>
+                            <br>
+                            <div class="container align-self-baseline  text-start">
+                              <br>
+                                <p class="text-white-75 mb-5" id="titulo" style="font-family: Open Sans; font-size: 250%;">Todo lo que necesita saber sobre los <b>Metaversos Empresariales</b></p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                <div class="container">
+                <div class="col-lg-5">
+                <div class="container-fluid">
+                <div class="container align-items-center">
                     <h1 class="text-white font-weight-bold" ><font color="#eba506" id="titulo" size="6">INSCRIBETE</font></h1>
                     @if(Session::has('mensaje'))
                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
@@ -72,46 +70,50 @@
                     </div>
                     @endif
 
-                    <form class="row g-3 needs-validation" action="{{route('datos')}}" method="post">
+                    <form class="row g-2 needs-validation align-items-center" action="{{route('datos')}}" method="post">
                      @csrf
-                      <div class="col-md-9">
+                      <div class="col-md-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-person-circle"></i></span>
                           <input type="text" class="form-control" id="lastname" name="nombre" placeholder="Nombre" required>
                         </div>
                       </div>
-                      <div class="col-md-9">
+                      <div class="col-md-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-building"></i></span>
                           <input type="text" class="form-control" id="empresa" name="empresa" placeholder="Empresa" required>
                         </div>
                       </div>
-                      <div class="col-md-9">
+                      <div class="col-md-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-briefcase-fill"></i></span>
                           <input type="text" class="form-control" id="cargo"  name="cargo" placeholder="Cargo" required>
                         </div>
                       </div>
-                      <div class="col-md-9">
+                      <div class="col-md-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-envelope"></i></span>
                           <input type="text" class="form-control" id="correo" name="correo" placeholder="correo@example.com" required>
                         </div>
                       </div>
-                      <div class="col-md-9">
+                      <div class="col-md-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-telephone-fill"></i></span>
                           <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono" required>
                         </div>
                       </div>
                       <div class="col-12">
-                        <button class="btn btn-warning" type="submit" style="color:#4b42bf">Enviar</button>
+                        <button class="btn btn-warning float-end" type="submit" style="color:#4b42bf">Enviar</button>
                       </div>
                     </form>
                    </div>
                   </div>
+               </div>
+               
            </div>
             <!---#############################-->
+            <br>
+          </div>
         </header>
         <!--##############################-->
          <!-- Content section 2-->
@@ -145,24 +147,25 @@
         <!-- Portfolio-->
         <div class="container col-auto p-2 text-center">
             <video autoplay controls style="width:50%; height: 50%; margin-right: 6px; margin-bottom: 10px;">
-              <source rc="https://www.youtube.com/embed/ABM5UJeILjo" type="video/mp4">
+              <source src="https://www.youtube.com/embed/ABM5UJeILjo" type="video/mp4">
             </video> 
             <br><br>
             <div class="row">
                 <div class="col-lg-8">
                     <div class="container px-4 px-lg-5 h-100">
-                        <div class="row gx-4 gx-lg-5 h-100 align-items-left; justify-content-left text-left">
+                        <div class="text-start">
                             <div class="col-lg-8 align-self-end">
-                                <h1 class="text-white font-weight-bold"align="left" ><font color="#4b42bf" id="titulo">Contenido:</font></h1>
-                            </div>
-                            <div class="col-lg-8 align-self-baseline justify-content-left">
-                                <h5 align="left"><font color="#95948c" size="3"><b>¿Qué son y cuáles</b> son sus aplicabilidades para las empresas, universidades, camaras de comercio?</font> </h5>
-                                <h5 align="left"><font color="#95948c" size="3"><b>¿Como diseñar espacios disruptivos</b> de formación en un METAVERSO personalizado para su organización?</font> </h5>
-                                <h5 align="left"><font color="#95948c" size="3"><b>¿Como construir ferias</b> internas o con proveedore para sus colaboradores o clientes en un METAVERSO?</font> </h5>
+                                <h1 class="font-weight-bold"><font color="#4b42bf" id="titulo">Contenido:</font></h1>
+                                <br>
+                              </div>
+                            <div class="justify-content-left">
+                                <h5 style="font-family: Open Sans; font-size: 113%; text-align:justify;"><b>¿Qué son y cuáles</b> son sus aplicabilidades para las empresas, universidades, camaras de comercio </h5>
+                                <h5 style="font-family: Open Sans; font-size: 113%; text-align:justify;"><b>¿Como diseñar espacios disruptivos</b> de formación en un METAVERSO personalizado para su organización?</h5>
+                                <h5 style="font-family: Open Sans; font-size: 113%; text-align:justify;"><b>¿Como construir ferias</b> internas o con proveedore para sus colaboradores o clientes en un METAVERSO?</h5>
                             </div>
                             <br>
                             <div class="align-self-baseline d-none d-sm-none d-md-block">
-                                <h5 align="left"><font color="#95948c" size="3" >Apoya:</font> </h5>
+                                <h5 style="font-family: Open Sans; font-size: 113%; text-align:justify;"><b>Apoya:</b></h5>
                                 <br>
                                 <img src="{{ asset('dist/assets/img/portfolio/events.png')}}" width="250px" height="50px" align="left">
                             </div> 
@@ -177,46 +180,40 @@
                     <h1 class="text-white font-weight-bold" align="left"><font color="#eba506" id="titulo" size="6">INSCRIBETE</font></h1>
                     <form  action="{{route('datos')}}" method="post" class="row g-3 needs-validation">
                     @csrf
-                      <div class="col-lg-9">
+                      <div class="col-lg-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-person-circle"></i></span>
                           <input type="text" class="form-control" id="lastname" name="nombre" placeholder="Nombre" required>
                         </div>
                       </div>
-                      <div class="col-lg-9">
+                      <div class="col-lg-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-building"></i></span>
                           <input type="text" class="form-control" id="empresa" name="empresa" placeholder="Empresa"required>
                         </div>
                       </div>
-                      <div class="col-lg-9">
+                      <div class="col-lg-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-briefcase-fill"></i></span>
                           <input type="text" class="form-control" id="cargo" name="cargo" placeholder="Cargo" required>
                         </div>
                       </div>
-                      <div class="col-lg-9">
+                      <div class="col-lg-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-envelope"></i></span>
                           <input type="text" class="form-control" id="correo" name="correo" placeholder="correo@example.com" required>
                         </div>
                       </div>
-                      <div class="col-lg-9">
+                      <div class="col-lg-12">
                         <div class="input-group has-validation">
                           <span class="input-group-text"><i class="bi bi-telephone-fill"></i></span>
                           <input type="text" class="form-control" id="telefono"  name="telefono" placeholder="Telefono" required>
                         </div>
                       </div>
                       <br>
-                      <div class="row">
-                        <div class="col-lg-6">
-
+                        <div class="col-lg-12">
+                          <button class="btn btn-warning float-end" type="submit" style="color:#4b42bf">Enviar</button>
                         </div>
-                        <div class="col-lg-2">
-                          <br>
-                          <button class="btn btn-warning" type="submit" style="color:#4b42bf">Enviar</button>
-                        </div>
-                      </div>
                     </form>
                    
                   </div>
